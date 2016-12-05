9af244a3d5197a4256d9da0e97980c623e5d633e
David Leseten <davidleseten@davids-mbp.attlocal.net>
2016-12-04 20:06:05 -0600

    Components

---
+++ komorebia-ui/src/Components/ActivityList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ActivityList extends Component{
+  render(){
+    return(
+      <div>
+        <h2>Activity List</h2>
+      </div>
+    )
+  }
+}
+
+export default ActivityList;
